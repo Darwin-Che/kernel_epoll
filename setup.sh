c92dee89547d63bfb4dbc674316b2ab8da9883e2
chezhaocheng ubuntu server <chezhaocheng@outlook.com>
2021-12-23 03:00:21 +0000

    modify setup

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo -n -e "Fetching buildroot source... \t\t\t"
+echo  -e "Fetching buildroot source... \t\t\t"
 
 if [[ -d buildroot ]]; then
 	echo "done!"
@@ -8,17 +8,18 @@ else
 	git clone https://github.com/buildroot/buildroot.git
 fi
 
-echo -n -e "Fetching kernel 5.15.11 ... \t\t\t"
+echo  -e "Fetching kernel 5.15.11 ... \t\t\t"
 if [[ -d linux ]]; then
 	echo "done!"
 else
-	echo -n -e "\tstart download ... \t\t\t"
-	curl "https://cdn.kernel.org/pub/linux/kernel/v5.x/linux-5.15.11.tar.xz"
-	echo -n -e "\tdownload complete ... \t\t\t"
-	echo -n -e "\tstart unzip... \t\t\t"
-	mkdir -p linux
-	tar -xf "linux-5.15.11.tar.xz" -C linux
-	echo -n -e "\tunzip complete ... \t\t\t"
+	echo  -e "\tstart download ... \t\t\t"
+	curl "https://cdn.kernel.org/pub/linux/kernel/v5.x/linux-5.15.11.tar.xz" --output "linux-5.15.11.tar.xz"
+	echo  -e "\tdownload complete ... \t\t\t"
+	echo  -e "\tstart unzip... \t\t\t"
+	tar -xf "linux-5.15.11.tar.xz" 
+	mv "linux-5.15.11" "linux"
+	echo  -e "\tunzip complete ... \t\t\t"
+	rm "linux-5.15.11.tar.xz"
 fi
 
 
